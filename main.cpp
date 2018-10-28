d8a0e9c9731867950cb25c283e6aefdea07a3659
timpcfan <lztsmail@gmail.com>
2018-10-28 15:19:38 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+/* 这是一个用于测试项目可行性的易碎玩具，只有基础游戏功能，不会有任何美化  */
+
 #include <QApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qDebug() << "Hello, 易碎玩具";
 
     return a.exec();
 }
