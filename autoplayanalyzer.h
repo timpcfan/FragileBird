8d874a91a94e76d23f3a792d4303563f9eb2f595
ZYTZJ <zjloveyt19@gmail.com>
2018-11-15 19:35:59 +0800

    对自动游戏机制的编写

--- autoplayanalyzer.h
+++ autoplayanalyzer.h
@@ -18,8 +18,15 @@ class AutoPlayAnalyzer : public QObject
 public:
     AutoPlayAnalyzer();
     bool canJump(GameData data); // 根据游戏信息判断是否可以跳跃
+    qreal calcuP(GameData data);//实现跳跃的自然计算在跳跃区跳跃的概率
 
 private:
+    const qreal alpha = 140;	//设置小鸟跳跃到管道之间的最短安全距离
+    const qreal beta = 30;	//设置管道的飞行的安全高度
+    const qreal delta = 10;	//设置小鸟跳出管道后安全的飞行距离
+    const qreal dh = 50; //设置安全飞行高度区间 y1-y0
+    const qreal lambda = 0.2; //设置均衡跳跃因子
+    const qreal gamma = 50  ;//概率计算的偏移量
 
 };
 
