73b237ee9c1f8461102c2643bd1a5c98dabc8966
ZYTZJ <zjloveyt19@gmail.com>
2018-11-15 19:53:09 +0800

    修改参数

--- autoplayanalyzer.h
+++ autoplayanalyzer.h
@@ -21,8 +21,8 @@ public:
     qreal calcuP(GameData data);//实现跳跃的自然计算在跳跃区跳跃的概率
 
 private:
-    const qreal alpha = 140;	//设置小鸟跳跃到管道之间的最短安全距离
-    const qreal beta = 30;	//设置管道的飞行的安全高度
+    const qreal alpha = 150;	//设置小鸟跳跃到管道之间的最短安全距离
+    const qreal beta = 35;	//设置管道的飞行的安全高度
     const qreal delta = 10;	//设置小鸟跳出管道后安全的飞行距离
     const qreal dh = 50; //设置安全飞行高度区间 y1-y0
     const qreal lambda = 0.2; //设置均衡跳跃因子
